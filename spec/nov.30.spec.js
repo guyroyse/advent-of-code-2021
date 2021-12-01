aac806a97715c577dcaa5942736f7c1209e4b815
Guy Royse <guy@guyroyse.com>
2021-12-01 18:41:50 -0500

    changed the name of the test

--- spec/nov.30.spec.js
+++ spec/nov.30.spec.js
@@ -1,7 +1,7 @@
 import { echo } from '../lib/advent.js'
 
 describe("November 30", () => {
-  it("runs", async () => {
+  it("computes the answer", async () => {
     let input = await fetchInput('2021-11-30.txt')
     let output = echo(input)
     expect(output).toBe(input)
