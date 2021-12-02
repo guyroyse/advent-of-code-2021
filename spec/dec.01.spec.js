2022aefb4ee019156ff34ca66264b5fda1efbb10
Guy Royse <guy@guyroyse.com>
2021-12-01 20:43:06 -0500

    part two solved... if a little janky

--- spec/dec.01.spec.js
+++ spec/dec.01.spec.js
@@ -1,6 +1,6 @@
-import { depthIncrease } from '../lib/advent.js'
+import { depthIncrease, depthIncreaseSliding } from '../lib/advent.js'
 
-describe("December 1", () => {
+describe("December 1 - Part 1", () => {
 
   it("returns 0 for no inputs", () => {
     expect(depthIncrease("")).toBe(0)
@@ -38,5 +38,42 @@ describe("December 1", () => {
     let input = await fetchInput('2021-12-01.txt')
     let output = depthIncrease(input)
     expect(output).toBe(1482)
+  })
+})
+
+describe("December 1 - Part 2", () => {
+
+  it("returns 0 for no inputs", () => {
+    expect(depthIncreaseSliding("")).toBe(0)
+  })
+
+  it("returns 0 for single input", () => {
+    expect(depthIncreaseSliding("200")).toBe(0)
+  })
+
+  it("returns 0 for a set of inputs", () => {
+    expect(depthIncreaseSliding("200 201 202")).toBe(0)
+  })
+
+  it("returns 1 for two sets of increasing inputs", () => {
+    expect(depthIncreaseSliding("200 201 202 203")).toBe(1)
+  })
+
+  it("returns 3 for four sets of increasing inputs", () => {
+    expect(depthIncreaseSliding("200 201 202 203 204 205")).toBe(3)
+  })
+
+  it("returns 0 for sets of decreasing inputs", () => {
+    expect(depthIncreaseSliding("200 199 198 197 196 195")).toBe(0)
+  })
+
+  it("returns 0 for sets of decreasing inputs", () => {
+    expect(depthIncreaseSliding("200 199 198 197 296 195")).toBe(1)
+  })
+
+  it("computes the answer", async () => {
+    let input = await fetchInput('2021-12-01.txt')
+    let output = depthIncreaseSliding(input)
+    expect(output).toBe(1518)
   })
 })
