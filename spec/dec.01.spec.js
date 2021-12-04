868683c3cc0eda2434a6a641bf06c49a33bfca5b
Guy Royse <guy@guyroyse.com>
2021-12-04 09:27:24 -0500

    reformating some tests

--- spec/dec.01.spec.js
+++ spec/dec.01.spec.js
@@ -1,79 +1,79 @@
 import { countDepthIncreases, countDepthIncreasesSliding } from '../lib/dec.01.js'
 
-describe("December 1 - Part 1", () => {
-
-  it("returns 0 for no inputs", () => {
-    expect(countDepthIncreases("")).toBe(0)
+describe("December 1", () => {
+  describe("Part 1", () => {
+    it("returns 0 for no inputs", () => {
+      expect(countDepthIncreases("")).toBe(0)
+    })
+  
+    it("returns 0 for single input", () => {
+      expect(countDepthIncreases("200")).toBe(0)
+    })
+  
+    it("returns 1 for two inputs with increasing difference", () => {
+      expect(countDepthIncreases("200 202")).toBe(1)
+    })
+  
+    it("returns 1 for multiple inputs with increasing difference", () => {
+      expect(countDepthIncreases("200 202 204 206")).toBe(3)
+    })
+  
+    it("returns 0 for two inputs with decreasing values", () => {
+      expect(countDepthIncreases("200 198")).toBe(0)
+    })
+  
+    it("returns 0 for multiple inputs with decreasing values", () => {
+      expect(countDepthIncreases("200 198 196 194")).toBe(0)
+    })
+  
+    it("returns 0 for multiple inputs with decreasing values", () => {
+      expect(countDepthIncreases("200 198 196 194")).toBe(0)
+    })
+  
+    it("returns the answer the multiple inputs with increasing and decreasing values", () => {
+      expect(countDepthIncreases("200 198 200 198 200 198 200 198")).toBe(3)
+    })
+  
+    it("computes the answer", async () => {
+      let input = await fetchInput('dec.01.txt')
+      let output = countDepthIncreases(input)
+      expect(output).toBe(1482)
+    })
   })
 
-  it("returns 0 for single input", () => {
-    expect(countDepthIncreases("200")).toBe(0)
-  })
+  describe("Part 2", () => {
+    it("returns 0 for no inputs", () => {
+      expect(countDepthIncreasesSliding("")).toBe(0)
+    })
 
-  it("returns 1 for two inputs with increasing difference", () => {
-    expect(countDepthIncreases("200 202")).toBe(1)
-  })
+    it("returns 0 for single input", () => {
+      expect(countDepthIncreasesSliding("200")).toBe(0)
+    })
 
-  it("returns 1 for multiple inputs with increasing difference", () => {
-    expect(countDepthIncreases("200 202 204 206")).toBe(3)
-  })
+    it("returns 0 for a set of inputs", () => {
+      expect(countDepthIncreasesSliding("200 201 202")).toBe(0)
+    })
 
-  it("returns 0 for two inputs with decreasing values", () => {
-    expect(countDepthIncreases("200 198")).toBe(0)
-  })
+    it("returns 1 for two sets of increasing inputs", () => {
+      expect(countDepthIncreasesSliding("200 201 202 203")).toBe(1)
+    })
 
-  it("returns 0 for multiple inputs with decreasing values", () => {
-    expect(countDepthIncreases("200 198 196 194")).toBe(0)
-  })
+    it("returns 3 for four sets of increasing inputs", () => {
+      expect(countDepthIncreasesSliding("200 201 202 203 204 205")).toBe(3)
+    })
 
-  it("returns 0 for multiple inputs with decreasing values", () => {
-    expect(countDepthIncreases("200 198 196 194")).toBe(0)
-  })
+    it("returns 0 for sets of decreasing inputs", () => {
+      expect(countDepthIncreasesSliding("200 199 198 197 196 195")).toBe(0)
+    })
 
-  it("returns the answer the multiple inputs with increasing and decreasing values", () => {
-    expect(countDepthIncreases("200 198 200 198 200 198 200 198")).toBe(3)
-  })
+    it("returns 0 for sets of increasing and decreasing inputs", () => {
+      expect(countDepthIncreasesSliding("200 199 198 197 296 195")).toBe(1)
+    })
 
-  it("computes the answer", async () => {
-    let input = await fetchInput('dec.01.txt')
-    let output = countDepthIncreases(input)
-    expect(output).toBe(1482)
-  })
-})
-
-describe("December 1 - Part 2", () => {
-
-  it("returns 0 for no inputs", () => {
-    expect(countDepthIncreasesSliding("")).toBe(0)
-  })
-
-  it("returns 0 for single input", () => {
-    expect(countDepthIncreasesSliding("200")).toBe(0)
-  })
-
-  it("returns 0 for a set of inputs", () => {
-    expect(countDepthIncreasesSliding("200 201 202")).toBe(0)
-  })
-
-  it("returns 1 for two sets of increasing inputs", () => {
-    expect(countDepthIncreasesSliding("200 201 202 203")).toBe(1)
-  })
-
-  it("returns 3 for four sets of increasing inputs", () => {
-    expect(countDepthIncreasesSliding("200 201 202 203 204 205")).toBe(3)
-  })
-
-  it("returns 0 for sets of decreasing inputs", () => {
-    expect(countDepthIncreasesSliding("200 199 198 197 196 195")).toBe(0)
-  })
-
-  it("returns 0 for sets of increasing and decreasing inputs", () => {
-    expect(countDepthIncreasesSliding("200 199 198 197 296 195")).toBe(1)
-  })
-
-  it("computes the answer", async () => {
-    let input = await fetchInput('dec.01.txt')
-    let output = countDepthIncreasesSliding(input)
-    expect(output).toBe(1518)
+    it("computes the answer", async () => {
+      let input = await fetchInput('dec.01.txt')
+      let output = countDepthIncreasesSliding(input)
+      expect(output).toBe(1518)
+    })
   })
 })
--- spec/dec.03.spec.js
+++ spec/dec.03.spec.js
@@ -1,79 +1,79 @@
 import { calculatePowerConsumption, calculateLifeSupportRating } from '../lib/dec.03.js'
 
-describe("December 3 - Part 1", () => {
+describe("December 3", () => {
+  describe("Part 2", () => {
+    it("computes expected values for all 1s", () => {
+      let result = calculatePowerConsumption("111111111111")
+      expect(result.gamma).toBe(4095)
+      expect(result.epsilon).toBe(0)
+      expect(result.power).toBe(0)
+    })
 
-  it("computes expected values for all 1s", () => {
-    let result = calculatePowerConsumption("111111111111")
-    expect(result.gamma).toBe(4095)
-    expect(result.epsilon).toBe(0)
-    expect(result.power).toBe(0)
+    it("computes expected values for multiple sets of all 1s", () => {
+      let result = calculatePowerConsumption("111111111111\n111111111111\n111111111111")
+      expect(result.gamma).toBe(4095)
+      expect(result.epsilon).toBe(0)
+      expect(result.power).toBe(0)
+    })
+
+    it("computes expected values for all 0s", () => {
+      let result = calculatePowerConsumption("000000000000")
+      expect(result.gamma).toBe(0)
+      expect(result.epsilon).toBe(4095)
+      expect(result.power).toBe(0)
+    })
+
+    it("computes expected values for multiple sets of all 0s", () => {
+      let result = calculatePowerConsumption("000000000000\n000000000000\n000000000000")
+      expect(result.gamma).toBe(0)
+      expect(result.epsilon).toBe(4095)
+      expect(result.power).toBe(0)
+    })
+
+    it("computes expected values for mix of 1s and 0s", () => {
+      let result = calculatePowerConsumption("111100001010")
+      expect(result.gamma).toBe(3850)
+      expect(result.epsilon).toBe(245)
+      expect(result.power).toBe(943250)
+    })
+
+    it("computes expected values for multiple sets of mixed of 1s and 0s", () => {
+      let result = calculatePowerConsumption("111100001010\n101011110000\n000010101111")
+      expect(result.gamma).toBe(2730)
+      expect(result.epsilon).toBe(1365)
+      expect(result.power).toBe(3726450)
+    })
+
+    it("computes the answer", async () => {
+      let input = await fetchInput('dec.03.txt')
+      let result = calculatePowerConsumption(input)
+      expect(result.gamma).toBe(1869)
+      expect(result.epsilon).toBe(2226)
+      expect(result.power).toBe(4160394)
+    })
   })
 
-  it("computes expected values for multiple sets of all 1s", () => {
-    let result = calculatePowerConsumption("111111111111\n111111111111\n111111111111")
-    expect(result.gamma).toBe(4095)
-    expect(result.epsilon).toBe(0)
-    expect(result.power).toBe(0)
-  })
+  describe("Part 2", () => {
+    it("computes expected values for multiple sets of mixed of 1s and 0s", () => {
+      let result = calculateLifeSupportRating("111100001010\n101011110000\n000010101111");
+      expect(result.lifeSupport).toBe(673750)
+      expect(result.o2).toBe(3850)
+      expect(result.co2).toBe(175)
+    })
 
-  it("computes expected values for all 0s", () => {
-    let result = calculatePowerConsumption("000000000000")
-    expect(result.gamma).toBe(0)
-    expect(result.epsilon).toBe(4095)
-    expect(result.power).toBe(0)
-  })
+    it("computes the answer with the sample data", () => {
+      let result = calculateLifeSupportRating("00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010");
+      expect(result.o2).toBe(23)
+      expect(result.co2).toBe(10)
+      expect(result.lifeSupport).toBe(230)
+    })
 
-  it("computes expected values for multiple sets of all 0s", () => {
-    let result = calculatePowerConsumption("000000000000\n000000000000\n000000000000")
-    expect(result.gamma).toBe(0)
-    expect(result.epsilon).toBe(4095)
-    expect(result.power).toBe(0)
-  })
-
-  it("computes expected values for mix of 1s and 0s", () => {
-    let result = calculatePowerConsumption("111100001010")
-    expect(result.gamma).toBe(3850)
-    expect(result.epsilon).toBe(245)
-    expect(result.power).toBe(943250)
-  })
-
-  it("computes expected values for multiple sets of mixed of 1s and 0s", () => {
-    let result = calculatePowerConsumption("111100001010\n101011110000\n000010101111")
-    expect(result.gamma).toBe(2730)
-    expect(result.epsilon).toBe(1365)
-    expect(result.power).toBe(3726450)
-  })
-
-  it("computes the answer", async () => {
-    let input = await fetchInput('dec.03.txt')
-    let result = calculatePowerConsumption(input)
-    expect(result.gamma).toBe(1869)
-    expect(result.epsilon).toBe(2226)
-    expect(result.power).toBe(4160394)
-  })
-})
-
-describe("December 3 - Part 2", () => {
-
-  it("computes expected values for multiple sets of mixed of 1s and 0s", () => {
-    let result = calculateLifeSupportRating("111100001010\n101011110000\n000010101111");
-    expect(result.lifeSupport).toBe(673750)
-    expect(result.o2).toBe(3850)
-    expect(result.co2).toBe(175)
-  })
-
-  it("computes the answer with the sample data", () => {
-    let result = calculateLifeSupportRating("00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010");
-    expect(result.o2).toBe(23)
-    expect(result.co2).toBe(10)
-    expect(result.lifeSupport).toBe(230)
-  })
-
-  it("computes the answer", async () => {
-    let input = await fetchInput('dec.03.txt')
-    let result = calculateLifeSupportRating(input)
-    expect(result.lifeSupport).toBe(4125600)
-    expect(result.o2).toBe(1719)
-    expect(result.co2).toBe(2400)
+    it("computes the answer", async () => {
+      let input = await fetchInput('dec.03.txt')
+      let result = calculateLifeSupportRating(input)
+      expect(result.lifeSupport).toBe(4125600)
+      expect(result.o2).toBe(1719)
+      expect(result.co2).toBe(2400)
+    })
   })
 })
