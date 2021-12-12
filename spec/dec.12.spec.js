1cb06baf92bd2cf5f9a3a8c9e79083ee2337f4b5
Guy Royse <guy@guyroyse.com>
2021-12-12 10:57:16 -0500

    plumbing for day 12

---
+++ spec/dec.12.spec.js
@@ -0,0 +1,16 @@
+import {} from "../lib/dec.12"
+
+const SAMPLE_INPUT = []
+const REAL_INPUT = []
+
+describe("December 12", () => {
+  describe("Part 1", () => {
+    xit("computes the sample answer", () => {})
+    xit("computes the real answer", () => {})
+  })
+  
+  describe("Part 2", () => {
+    xit("computes the sample answer", () => {})
+    xit("computes the real answer", () => {})
+  })
+})
