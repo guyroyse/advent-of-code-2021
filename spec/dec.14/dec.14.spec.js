f395094249796ebd0666d604c81f403575a513bf
Guy Royse <guy@guyroyse.com>
2021-12-14 14:37:12 -0500

    part 1 code, part 2 failure

--- spec/dec.14/dec.14.spec.js
+++ spec/dec.14/dec.14.spec.js
@@ -1,14 +1,59 @@
-import {} from "../../lib/dec.14"
+import { sampleTemplate, sampleRules, realTemplate, realRules } from "./data" 
+import { applyRules, applyRulesQuickly, createHistogram, subtractMaxMin, applyRulesOnce, shingleString } from "../../lib/dec.14"
 
 describe("December 14", () => {
 
+  it("shingles a string", () => {
+    let shingles = shingleString('ABCDEF')
+    expect(shingles).toEqual([ 'AB', 'BC', 'CD', 'DE', 'EF' ])
+  })
+
+  it("applies a rule to a polymer template", () => {
+    let polymer = applyRulesOnce(sampleTemplate, sampleRules)
+    expect(polymer).toBe('NCNBCHB')
+  })
+
+  it("applies a rule to a polymer template multiple times", () => {
+    let polymer = applyRules(sampleTemplate, sampleRules, 10)
+    expect(polymer).toHaveLength(3073)
+  })
+
+  it("creates a histogram of a polymer", () => {
+    let histogram = createHistogram("AAAABBBCCD")
+    expect(histogram.A).toBe(4)
+    expect(histogram.B).toBe(3)
+    expect(histogram.C).toBe(2)
+    expect(histogram.D).toBe(1)
+  })
+
+  it("subtracts the max and the min from a histogram", () => {
+    let histogram = { A: 4, B: 3, C: 2, D: 1 }
+    let difference = subtractMaxMin(histogram)
+    expect(difference).toBe(3)
+  })
+
   describe("Part 1", () => {
-    xit("computes the sample answer", () => {})
-    xit("computes the real answer", () => {})
+    it("computes the sample answer", () => {
+      let polymer = applyRules(sampleTemplate, sampleRules, 10)
+      let histogram = createHistogram(polymer)
+      let difference = subtractMaxMin(histogram)
+      expect(difference).toBe(1588)
+    })
+
+    it("computes the real answer", () => {
+      let polymer = applyRules(realTemplate, realRules, 10)
+      let histogram = createHistogram(polymer)
+      let difference = subtractMaxMin(histogram)
+      expect(difference).toBe(2621)
+    })
   })
   
   describe("Part 2", () => {
-    xit("computes the sample answer", () => {})
+    it("computes the sample answer", () => {
+      let histogram = applyRulesQuickly(sampleTemplate, sampleRules, 40)
+      let difference = subtractMaxMin(histogram)
+      expect(difference).toBe(1588)
+    })
     xit("computes the real answer", () => {})
   })
 })
