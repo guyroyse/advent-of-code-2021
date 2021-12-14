90a0366e6f718173578c3ebeef265095c3347841
Guy Royse <guy@guyroyse.com>
2021-12-14 12:29:58 -0500

    prestream boilerplate

---
+++ spec/dec.14/dec.14.spec.js
@@ -0,0 +1,14 @@
+import {} from "../../lib/dec.14"
+
+describe("December 14", () => {
+
+  describe("Part 1", () => {
+    xit("computes the sample answer", () => {})
+    xit("computes the real answer", () => {})
+  })
+  
+  describe("Part 2", () => {
+    xit("computes the sample answer", () => {})
+    xit("computes the real answer", () => {})
+  })
+})
