44cbe3cf5c386ae8cc6af2aca51b436e570e057e
Guy Royse <guy@guyroyse.com>
2021-12-13 19:22:24 -0500

    removed stray number

--- spec/dec.13/dec.13.spec.js
+++ spec/dec.13/dec.13.spec.js
@@ -2,7 +2,7 @@ import {} from "../../lib/dec.13"
 
 describe("December 13", () => {
   describe("Part 1", () => {
-    xit("computes the sample answer1", () => {})
+    xit("computes the sample answer", () => {})
     xit("computes the real answer", () => {})
   })
   
